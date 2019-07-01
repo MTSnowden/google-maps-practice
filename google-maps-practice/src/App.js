a6afe0a45176845dde138731d1a0c7071e64f441
Michael Snowden <mtsnowden@gmail.com>
2019-07-01 13:48:51 -0500

    map working

--- google-maps-practice/src/App.js
+++ google-maps-practice/src/App.js
@@ -1,24 +1,27 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { GoogleMap, withScriptjs, withGoogleMap } from 'react-google-maps';
 
+function Map() {
+  return (
+    <GoogleMap
+      defaultZoom={10}
+      defaultCenter={{ lat: 30.267153, lng: -97.743057 }}
+    />
+  );
+ }
+
+ const WrappedMap = withScriptjs(withGoogleMap(Map));
+ 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{width: '100vw', height: '100vh'}} >
+      <WrappedMap
+        googleMapURL={"https://maps.googleapis.com/maps/api/js?key=AIzaSyD8--ovOhB1SIgxHIpXhoZECQ0sOIpiMPg&callback=initMap"}
+        loadingElement={<div style={{ height: "100%" }} />}
+        containerElement={<div style={{ height: "100%" }} />}
+        mapElement={<div style={{ height: "100%" }} />}   
+      />
     </div>
   );
 }
